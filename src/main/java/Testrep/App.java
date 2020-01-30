c67ce0ddeb2f6209fa167b7d5cbcaa2dcc205935
Manvitha Challagondla <mchallagondla@hostanalytics.com>
2020-01-30 11:32:51 +0530

    ME branching out

--- src/main/java/Testrep/App.java
+++ src/main/java/Testrep/App.java
@@ -5,7 +5,7 @@ package Testrep;
 
 public class App {
     public String getGreeting() {
-        return "Hello from master.";
+        return "Hello from MANVITHA/DEV.";
     }
 
     public static void main(String[] args) {
